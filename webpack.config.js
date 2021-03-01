1d49a48d62afd55a6d04f4cb4b149683f6918441
alejandrotoledoweb <atoledofr@gmail.com>
2021-03-01 15:37:24 -0500

    add start files and set up webpack with some dependencies

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+      {
+
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: 'asset/resource',
+
+      },
+      {
+
+        test: /\.(csv|tsv)$/i,
+
+        use: ['csv-loader'],
+
+      },
+
+      {
+
+        test: /\.xml$/i,
+
+        use: ['xml-loader'],
+
+      },
+
+    ],
+
+  },
+};
