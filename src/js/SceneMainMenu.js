85693b56a99a215be798d3d0148952c791a2f2f5
alejandrotoledoweb <atoledofr@gmail.com>
2021-03-02 15:42:00 -0500

    add update function to SceneMian.js and other dependencies

--- src/js/SceneMainMenu.js
+++ src/js/SceneMainMenu.js
@@ -2,8 +2,19 @@ class SceneMainMenu extends Phaser.Scene {
   constructor() {
     super({ key: "SceneMainMenu" });
   }
+  preload() {
+    this.load.image("sprBtnPlay", "content/sprBtnPlay.png");
+    this.load.image("sprBtnPlayHover", "content/sprBtnPlayHover.png");
+    this.load.image("sprBtnPlayDown", "content/sprBtnPlayDown.png");
+    this.load.image("sprBtnRestart", "content/sprBtnRestart.png");
+    this.load.image("sprBtnRestartHover", "content/sprBtnRestartHover.png");
+    this.load.image("sprBtnRestartDown", "content/sprBtnRestartDown.png");
 
-  create() {
+    this.load.audio("sndBtnOver", "content/sndBtnOver.wav");
+    this.load.audio("sndBtnDown", "content/sndBtnDown.wav");
+  }
+
+  create = () => {
     this.scene.start("SceneMain");
   }
 }
