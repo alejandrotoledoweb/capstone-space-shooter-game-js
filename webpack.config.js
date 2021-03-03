76e9ccb26a8b0f6039dc262257fda93f3169f7c4
alejandrotoledoweb <atoledofr@gmail.com>
2021-03-03 16:37:02 -0500

    modify webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    'production-dependencies': ['phaser'],
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
   mode: 'development',
   devServer: {
     contentBase: './dist',
@@ -13,7 +20,30 @@ module.exports = {
   module: {
 
     rules: [
-
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
       {
 
         test: /\.css$/i,
@@ -29,11 +59,10 @@ module.exports = {
 
       },
       {
-
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-
-        type: 'asset/resource',
-
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
       },
       {
 
@@ -52,6 +81,30 @@ module.exports = {
       },
 
     ],
-
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'index.html'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets/'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      name: 'production-dependencies',
+      filename: 'production-dependencies.bundle.js',
+    },
   },
 };
