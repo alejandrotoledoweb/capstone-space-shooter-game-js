841bf24a39a02b449e8d44ab8e72a85e19feacfe
alejandrotoledoweb <atoledofr@gmail.com>
2021-03-04 12:18:27 -0500

    create gameScenes file with just the create and update functions

---
+++ src/scenes/GameScenes.js
@@ -0,0 +1,32 @@
+import Phaser from 'phaser';
+import Player from '../entities/Player';
+import UFO from '../entities/UFO';
+import Ninja from '../entities/Ninja';
+import Paranoid from '../entities/Paranoid';
+import Saboteur from '../entities/Saboteur';
+import Lightning from '../entities/Lightning';
+import ScrollingBackground from '../objects/ScrollingBackground';
+
+export default class GameScene extends Phaser.Scene {
+  constructor() {
+    super('Game');
+  }
+
+
+  preload() {
+
+
+  }
+
+
+  create() {
+
+
+  }
+
+
+  update() {
+
+    
+  }
+}
